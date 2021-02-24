02bcff3cfdc36dd458bb5013a5f59b4204be4a5f
Lotan <lotan.mash@gmail.com>
2021-02-24 21:00:55 +0200

    menu style added

--- js/apps/email/pages/email-details.cmp.js
+++ js/apps/email/pages/email-details.cmp.js
@@ -1,17 +1,21 @@
 import { emailService } from '../services/email.service.js'
+
+
+// SMART COMPONENT!
+
 export default {
     name: 'email-details',
     template: `
     <section class="email-details" v-if="email">
         <router-link to="/email">Back to inbox</router-link>
-        <button @click="remove(email.id)">Delete</button>
-        <div class="">
+        <button @click="removeEmail(email.id)">Delete</button>
+        <!-- <div class=""> -->
             <h1 class="subject">Subject: {{email.subject}}</h1>
             <p class="body">{{email.body}}</p>
             <p>{{email.sentAt}}</p>
             <button><--Reply</button>
             <button>Forward--></button>
-        </div>
+        <!-- </div> -->
     </section>
     `,
     data() {
@@ -20,10 +24,9 @@ export default {
         }
     },
     methods: {
-        remove(emailId) {
-            console.log('first remove');
-            console.log(this.email.id);
-            this.$emit('remove', emailId);
+        removeEmail(emailId) {
+            emailService.remove(emailId)
+                .then(this.$router.push('/email'))
         }
     },
     created() {
