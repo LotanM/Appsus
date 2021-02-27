0dbb7c2fdfac45f50c55e93d6cbe967ab71e3246
Lotan <lotan.mash@gmail.com>
2021-02-27 12:02:38 +0200

    changed homepage

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -7,11 +7,12 @@ export default {
             <div class="profile-img"><img src="https://ca.slack-edge.com/T01FLU17LTC-U01HHR0MNPR-98663c8ba16b-512" alt=""></div> 
             <div class="profile-img"><img src="https://ca.slack-edge.com/T01FLU17LTC-U01FJ1LQY6R-6e45df534775-512" alt=""></div> 
         </div> -->
-        <div class="hero-txt-container">
-            <h3>Set meetings using<span> Email</span></h3>
-            <h3><span>Take a note</span> using notes</h3>
+        <div class="hero-inner-container">
+            <h1>Welcome to Appsus</h1>
+            <h3>Set meetings via<span> Email</span></h3>
+            <h3><span>Write</span> notes</h3>
             <h3>Manage your own<span> Book Shop</span></h3>
-            <h3>Do all that using Appsus!</h3>
+            <h3>Do all of that using Appsus!</h3>
             <router-link to="/about">
                  <button>About Us</button>
             </router-link>
