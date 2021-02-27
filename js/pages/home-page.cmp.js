20e64aa568b06731c8aa1d3918a3a42358f043e2
Lotan <lotan.mash@gmail.com>
2021-02-27 03:23:19 +0200

    working on home-page

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -7,7 +7,7 @@ export default {
             <div class="profile-img"><img src="https://ca.slack-edge.com/T01FLU17LTC-U01HHR0MNPR-98663c8ba16b-512" alt=""></div> 
             <div class="profile-img"><img src="https://ca.slack-edge.com/T01FLU17LTC-U01FJ1LQY6R-6e45df534775-512" alt=""></div> 
         </div> -->
-        <div class="hero-txt">
+        <div class="hero-txt-container">
             <h3>Set meetings using<span> Email</span></h3>
             <h3><span>Take a note</span> using notes</h3>
             <h3>Manage your own<span> Book Shop</span></h3>
